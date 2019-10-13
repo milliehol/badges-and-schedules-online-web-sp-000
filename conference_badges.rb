c15c3af8d9fcfb841fe6db603b191eb3fc1726d4
Carla Holcomb <carla.programmer@gmail.com>
2019-10-13 16:43:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+
+end
+
+def batch_badge_creator(array)
+   narray = []
+  array.each do |name|
+  narray << badge_maker(name)
+    end
+return narray
+end
+
+def assign_rooms(array)
+  narray = []
+ array.each do |name|
+ narray << badge_maker(name)
+   end
+return narray
+
+end
+
+def printer
+
+end
